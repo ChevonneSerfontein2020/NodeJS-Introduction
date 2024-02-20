49f0b1f06f49f5c7d4fd46f2155aa2b9cba1f997
chevonnes@runninghill.co.za <chevonnes@runninghill.co.za>
2024-02-20 13:33:34 +0200

    completed web server setup

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const { error } = require('console')
+const fs = require('fs') // file handling
 const http = require('http')
 const port = 3000
 
 // create server object
-const server = http.createServer(function (req, res){
-    Response.write('Hello!')
+const server = http.createServer(function (req, res){ // read the index.html file in the current directory
+    res.writeHead(200, {'Content-Type': 'text/html'}) // 200 is a good code, tells server what content it will render
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404) // 404 is a bad code
+            res.write('Error: File not found!')
+        } else {
+            res.write(data)
+        }
+    res.end() // close service connection    
+    })
 })
 
 // set server to listen

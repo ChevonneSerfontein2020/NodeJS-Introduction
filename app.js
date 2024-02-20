bcf5ee2b64f96ca521c84d3341bf9b6ac829d7af
chevonnes@runninghill.co.za <chevonnes@runninghill.co.za>
2024-02-20 12:32:27 +0200

    Added Hello World Message

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+const { error } = require('console')
 const http = require('http')
 const port = 3000
 
+// create server object
 const server = http.createServer(function (req, res){
-    
+    Response.write('Hello!')
+})
+
+// set server to listen
+server.listen(port, function(error) {
+    if (error) { // if server was unable to connect to port
+        console.log('Something went wrong. Please try again.', error)
+    } else { // if server successfully connected
+        console.log('Server is listening on port ' + port)
+    }
 })
